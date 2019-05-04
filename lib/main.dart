adafaac6d3739869f6a118f7445a41f73976b337
aochuih <aochuih@hotmail.com>
2019-05-04 09:51:45 +0800

    add theme demo

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      theme: ThemeData(
+        // 定义默认亮度和颜色
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+
+        // 定义默认字体
+        fontFamily: 'Montserrat',
+
+        // 定义默认文字主题，包括字体大小、字体、样式
+        textTheme: TextTheme(
+          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -40,10 +56,30 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('当前计数：'),
+            Container(
+                height: 44,
+                width: 100,
+                color: Colors.amber,
+                child: Text('当前计数：')
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.display1,
+            ),
+            DefaultTextStyle(
+              style: TextStyle(
+                color: Colors.yellow,
+                fontSize: 20,
+              ),
+              child: Column(
+                children: <Widget>[
+                  Text('Text with default style'),
+                  Text(
+                    'Text with a background color',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
