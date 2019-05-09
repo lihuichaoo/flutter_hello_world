6356681868e3411ac07ed72b69756dfdcfe0fe97
aochuih <aochuih@hotmail.com>
2019-05-09 22:28:51 +0800

    AnimatedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(LogoApp());
 
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
+    );
+  }
+}
+
 class LogoApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
 }
@@ -17,24 +34,17 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
     animation = Tween<double>(begin: 0, end: 300).animate(controller);
-    animation.addListener(() {
-        setState(() {
-          // The state that has changed here is the animation object’s value.
-        });
+    /* animation.addListener(() {
+      setState(() {
+        // The state that has changed here is the animation object’s value.
       });
+    }); */
     controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        height: animation.value,
-        width: animation.value,
-        child: FlutterLogo(),
-      ),
-    );
+    return AnimatedLogo(animation: animation);
   }
 
   @override
