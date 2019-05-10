fe74cc9d4f32e7e0874d3ce20bd1bd2b40fc676a
aochuih <aochuih@hotmail.com>
2019-05-10 23:49:42 +0800

    PointerEvent

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,86 @@
-import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/physics.dart';
+import 'package:flutter/gestures.dart';
 
-void main() => runApp(LogoApp());
+void main() => runApp(MyApp());
 
-class AnimatedLogo extends AnimatedWidget {
-  static final _sizeTween = Tween<double>(begin: 0, end: 300);
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
 
-  AnimatedLogo({Key key, Animation<double> animation})
-      : super(key: key, listenable: animation);
-
+  @override
   Widget build(BuildContext context) {
-    final Animation<double> animation = listenable;
-    return Center(
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        height: _sizeTween.evaluate(animation),
-        width: _sizeTween.evaluate(animation),
-        child: FlutterLogo(),
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: Text(_title)),
+        body: MyStatefulWidget(),
       ),
     );
   }
 }
 
-class LogoApp extends StatefulWidget {
-  _LogoAppState createState() => _LogoAppState();
-}
-
-class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
-  Animation<double> animation;
-  AnimationController controller;
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
 
   @override
-  void initState() {
-    super.initState();
-    controller = AnimationController.unbounded(
-        duration: const Duration(seconds: 10),
-        vsync: this);
-    controller.animateWith(
-        GravitySimulation(
-            10, /// 加速度
-            0, /// 初始距离
-            1000, /// 距离
-            1 /// 速度
-        ));
-    animation = controller;
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _enterCounter = 0;
+  int _exitCounter = 0;
+  double x = 0.0;
+  double y = 0.0;
+
+  void _incrementCounter(PointerDownEvent details) {
+    setState(() {
+      _enterCounter++;
+    });
+  }
+  void _decrementCounter(PointerUpEvent details) {
+    setState(() {
+      _exitCounter++;
+    });
+  }
+  void _updateLocation(PointerMoveEvent details) {
+    setState(() {
+      x = details.position.dx;
+      y = details.position.dy;
+    });
+  }
+  void _cancelEventHandler(PointerCancelEvent details) {
+    setState(() {
+      _exitCounter += 10;
+    });
   }
 
   @override
-  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
+  Widget build(BuildContext context) {
+    return Center(
+      child: ConstrainedBox(
+        constraints: BoxConstraints.tight(Size(300.0, 200.0)),
+        child: Listener(
+          onPointerDown: _incrementCounter,
+          onPointerMove: _updateLocation,
+          onPointerUp: _decrementCounter,
+          onPointerCancel: _cancelEventHandler,
+          child: Container(
+            color: Colors.lightBlueAccent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('You have pointed at this box this many times:'),
+                Text('$_enterCounter Entries\n$_exitCounter Exits',
+                  style: Theme.of(context).textTheme.display1,
+                ),
+                Text('The cursor is here: (${x.toStringAsFixed(2)}, ${y.toStringAsFixed(2)})',),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
+
 
