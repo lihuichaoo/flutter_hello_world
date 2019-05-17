43290fbe576ceeb43f8e02909c6de4bf219b6841
aochuih <aochuih@hotmail.com>
2019-05-17 09:22:44 +0800

    BLoC demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,161 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
 
-// One simple action: Increment
-enum Actions { Increment }
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-// The reducer, which takes the previous count and increments it in response
-// to an Increment action.
-int counterReducer(int state, dynamic action) {
-  return action == Actions.Increment ? state+1 : state;
+class SimpleBlocDelegate extends BlocDelegate {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    super.onEvent(bloc, event);
+    print(event);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
+    super.onError(bloc, error, stacktrace);
+    print(error);
+  }
 }
 
 void main() {
-  // Create your store as a final variable in a base Widget. This works better
-  // with Hot Reload than creating it directly in the `build` function.
-  final store = Store<int>(counterReducer, initialState: 0);
-
-  runApp(FlutterReduxApp(
-    title: 'Flutter Redux Demo',
-    store: store,
-  ));
+  BlocSupervisor().delegate = SimpleBlocDelegate();
+  runApp(App());
 }
 
-class FlutterReduxApp extends StatelessWidget {
-  final Store<int> store;
-  final String title;
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AppState();
+}
 
-  FlutterReduxApp({Key key, this.store, this.title}) : super(key: key);
+class _AppState extends State<App> {
+  final CounterBloc _counterBloc = CounterBloc();
+  final ThemeBloc _themeBloc = ThemeBloc();
 
   @override
   Widget build(BuildContext context) {
-    // The StoreProvider should wrap your MaterialApp or WidgetsApp. This will
-    // ensure all routes have access to the store.
-    return StoreProvider<int>(
-      // Pass the store to the StoreProvider. Any ancestor `StoreConnector`
-      // Widgets will find and use this value as the `Store`.
-      store: store,
-      child: MaterialApp(
-        theme: ThemeData.dark(),
-        title: title,
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text(title),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'You have pushed the button this many times:',
-                ),
-                // . . . .
-                // Connect the Store to a Text Widget that renders the current
-                // count.
-                //
-                // We'll wrap the Text Widget in a `StoreConnector` Widget. The
-                // `StoreConnector` will find the `Store` from the nearest
-                // `StoreProvider` ancestor, convert it into a String of the
-                // latest count, and pass that String  to the `builder` function
-                // as the `count`.
-                //
-                // Every time the button is tapped, an action is dispatched and
-                // run through the reducer. After the reducer updates the state,
-                // the Widget will be automatically rebuilt with the latest
-                // count. No need to manually manage subscriptions or Streams!
-                StoreConnector<int, String>(
-                  converter: (store) => store.state.toString(),
-                  builder: (context, count) {
-                    return Text(
-                      count,
-                      style: Theme.of(context).textTheme.display1,
-                    );
-                  },
-                )
-              ],
-            ),
-          ),
-          // Connect the Store to a FloatingActionButton. In this case, we'll
-          // use the Store to build a callback that with dispatch an Increment
-          // Action.
-          //
-          // Then, we'll pass this callback to the button's `onPressed` handler.
-          floatingActionButton: StoreConnector<int, VoidCallback>(
-            converter: (store) {
-              // Return a `VoidCallback`, which is a fancy name for a function
-              // with no parameters. It only dispatches an Increment action.
-              return () => store.dispatch(Actions.Increment);
-            },
-            builder: (context, callback) {
-              return FloatingActionButton(
-                // Attach the `callback` to the `onPressed` attribute
-                onPressed: callback,
-                tooltip: 'Increment',
-                child: Icon(Icons.add),
-              );
-            },
-          ),
-        ),
+    return BlocProviderTree(
+      blocProviders: [
+        BlocProvider<CounterBloc>(bloc: _counterBloc),
+        BlocProvider<ThemeBloc>(bloc: _themeBloc)
+      ],
+      child: BlocBuilder(
+        bloc: _themeBloc,
+        builder: (_, ThemeData theme) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            home: CounterPage(),
+            theme: theme,
+          );
+        },
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _counterBloc.dispose();
+    _themeBloc.dispose();
+    super.dispose();
+  }
+}
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final CounterBloc _counterBloc = BlocProvider.of<CounterBloc>(context);
+    final ThemeBloc _themeBloc = BlocProvider.of<ThemeBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter')),
+      body: BlocBuilder<CounterEvent, int>(
+        bloc: _counterBloc,
+        builder: (BuildContext context, int count) {
+          return Center(
+            child: Text(
+              '$count',
+              style: TextStyle(fontSize: 24.0),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                _counterBloc.dispatch(CounterEvent.increment);
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.remove),
+              onPressed: () {
+                _counterBloc.dispatch(CounterEvent.decrement);
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.update),
+              onPressed: () {
+                _themeBloc.dispatch(ThemeEvent.toggle);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+enum CounterEvent { increment, decrement }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  @override
+  int get initialState => 0;
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.decrement:
+        yield currentState - 1;
+        break;
+      case CounterEvent.increment:
+        yield currentState + 1;
+        break;
+    }
+  }
+}
+
+enum ThemeEvent { toggle }
+
+class ThemeBloc extends Bloc<ThemeEvent, ThemeData> {
+  @override
+  ThemeData get initialState => ThemeData.light();
+
+  @override
+  Stream<ThemeData> mapEventToState(ThemeEvent event) async* {
+    switch (event) {
+      case ThemeEvent.toggle:
+        yield currentState == ThemeData.dark()
+            ? ThemeData.light()
+            : ThemeData.dark();
+        break;
+    }
   }
 }
